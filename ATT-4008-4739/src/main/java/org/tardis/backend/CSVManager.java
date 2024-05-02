f61f3f172a253bea66425b70dc11f0be74ef449b
Lydia <56268711+AngelGketsis@users.noreply.github.com>
2024-05-02 22:36:05 +0300

    Evie is stupid

--- ATT-4008-4739/src/main/java/org/tardis/backend/CSVManager.java
+++ ATT-4008-4739/src/main/java/org/tardis/backend/CSVManager.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.tardis.backend;
 
 import java.io.*;
 
@@ -30,30 +30,20 @@ public class CSVManager {
     }
 
     public static void main(String[] args) {
-        String inputFile = "../countries_archive/countries.csv"; // Path to input CSV file
+        String inputFile = "../src/main/resources/data/countries.csv"; // Path to input CSV file
 
 
-        String[] outputFile = {"geo_data.csv", "currency_data.csv"}; // Path to output CSV file
-        int[] columnsToCopy = {1, 4, 22, 23, 6, 7}; // Indices of columns to copy (0-based)
-        for(int i = )
-
-
+        String[] outputFile = {"../src/main/resources/data/geo_data.csv", "../src/main/resources/data/currency_data.csv", "../src/main/resources/data/development_data.csv", "../src/main/resources/data/region_data.csv"}; // Paths to output CSV files
+        int[][] columnsToCopy = {{1, 4, 22, 23, 6, 7}, {1, 8, 9}, {1, 17, 18, 19, 20, 21}, {1, 10, 11, 12, 13, 14}}; // Indices of columns to copy (0-based)
+        for(int i = 0; i < outputFile.length; i++)
+        {
             try {
-                copyColumns(inputFile, outputFile, columnsToCopy);
+                copyColumns(inputFile, outputFile[i], columnsToCopy[i]);
                 System.out.println("Columns copied successfully!");
             } catch (IOException e) {
                 System.err.println("Error: " + e.getMessage());
             }
-
-        outputFile = "currency_data.csv"; // Path to output CSV file
-
-        try {
-            copyColumns(inputFile, outputFile, new int[] {1, 8, 9});
-            System.out.println("Columns copied successfully!");
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
         }
-
     }
 
 }
--- ATT-4008-4739/src/main/java/org/tardis/frontend/controler.java
+++ ATT-4008-4739/src/main/java/org/tardis/frontend/controler.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package org.tardis.frontend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
