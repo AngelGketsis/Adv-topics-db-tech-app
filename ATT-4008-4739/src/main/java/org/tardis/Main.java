69aafb8f5a0528aa090324300689d38aa627d8ef
Evie <iviballou@gmail.com>
2024-05-03 01:22:50 +0300

    moved csvs, started on frontend

--- ATT-4008-4739/src/main/java/org/tardis/Main.java
+++ ATT-4008-4739/src/main/java/org/tardis/Main.java
@@ -1,7 +1,11 @@
 package org.tardis;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
