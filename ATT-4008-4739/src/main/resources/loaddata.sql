8963932c595c05e590a2d71732da90898421c0ff
Lydia <56268711+AngelGketsis@users.noreply.github.com>
2024-05-24 01:49:34 +0300

    Updated loaddatasql

--- ATT-4008-4739/src/main/resources/loaddata.sql
+++ ATT-4008-4739/src/main/resources/loaddata.sql
@@ -1,4 +1,4 @@
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/geo_data.csv'
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/geo_data.csv'
 INTO TABLE Countries
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -6,7 +6,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (ISO3, Display_Name, Area_SqKm, Population, Capital, Continent);
 
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/currency_data.csv'
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/currency_data.csv'
 INTO TABLE Currencies
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -14,7 +14,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (ISO3, CurrencyCode, CurrencyName);
 
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/region_data.csv'
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/region_data.csv'
 INTO TABLE CountryRegionDetails
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -22,7 +22,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (ISO3, Phone, Region_Code, Region_Name, Subregion_Code, Subregion_Name, Intermediate_Region_Code, Intermediate_Region_Name);
 
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/development_data.csv'
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/development_data.csv'
 INTO TABLE CountryDevelopmentStatus
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -37,37 +37,37 @@ SET
 
 
 -- Load data into ASTC_Data table
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/ASTC_data.csv'
-    INTO TABLE ASTC_Data
-    FIELDS TERMINATED BY ','
-    ENCLOSED BY '"'
-    LINES TERMINATED BY '\n'
-    IGNORE 1 ROWS
-    (ISO3, Year, Value);
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/ASTC_data.csv'
+INTO TABLE ASTC_Data
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(ISO3, Year, Value);
 
 -- Load data into ASTC_Data table
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/CRDF_data.csv'
-    INTO TABLE CRDF_Data
-    FIELDS TERMINATED BY ','
-    ENCLOSED BY '"'
-    LINES TERMINATED BY '\n'
-    IGNORE 1 ROWS
-    (ISO3, Year, Value);
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/CRDF_data.csv'
+INTO TABLE CRDF_Data
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(ISO3, Year, Value);
 
 -- Load data into ASTC_Data table
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/FC_data.csv'
-    INTO TABLE FC_Data
-    FIELDS TERMINATED BY ','
-    ENCLOSED BY '"'
-    LINES TERMINATED BY '\n'
-    IGNORE 1 ROWS
-    (ISO3, Year, Value);
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/FC_data.csv'
+INTO TABLE FC_Data
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(ISO3, Year, Value);
 
 -- Load data into ASTC_Data table
-LOAD DATA INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/LCA_data.csv'
-    INTO TABLE LCA_Data
-    FIELDS TERMINATED BY ','
-    ENCLOSED BY '"'
-    LINES TERMINATED BY '\n'
-    IGNORE 1 ROWS
-    (ISO3, Year, Value);
+LOAD DATA LOCAL INFILE 'E:/CS UOI/Earino/Advanced Tech/ATT-4008-4739/target/classes/data/LCA_data.csv'
+INTO TABLE LCA_Data
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(ISO3, Year, Value);
