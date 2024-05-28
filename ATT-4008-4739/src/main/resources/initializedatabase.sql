f55d6e33e69b6905fd16d7e635292988708c6658
Lydia <56268711+AngelGketsis@users.noreply.github.com>
2024-05-28 23:17:21 +0300

    Reworked Initializedatabase and loaddata sql file

--- ATT-4008-4739/src/main/resources/initializedatabase.sql
+++ ATT-4008-4739/src/main/resources/initializedatabase.sql
@@ -1,11 +1,17 @@
+
+-- Drop database if it exists
+DROP DATABASE IF EXISTS CountryDatabase;
+
 -- Create the database
-CREATE DATABASE CountryDatabase;
+CREATE DATABASE IF NOT EXISTS CountryDatabase;
 
 -- Use the newly created database
 USE CountryDatabase;
 
+DROP TABLE IF EXISTS Countries;
+
 -- Create Countries table
-CREATE TABLE Countries (
+CREATE TABLE IF NOT EXISTS Countries (
     ISO3 CHAR(15) PRIMARY KEY,
     Display_Name VARCHAR(255),
     Area_SqKm DECIMAL(18, 2),
@@ -14,8 +20,10 @@ CREATE TABLE Countries (
     Continent VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS Currencies;
+
 -- Create Currencies table
-CREATE TABLE Currencies (
+CREATE TABLE IF NOT EXISTS Currencies (
     ISO3 CHAR(15),
     CurrencyCode VARCHAR(3),
     CurrencyName VARCHAR(255),
@@ -23,8 +31,10 @@ CREATE TABLE Currencies (
     FOREIGN KEY(ISO3) REFERENCES Countries(ISO3)
 );
 
+DROP TABLE IF EXISTS CountryRegionDetails;
+
 -- Create CountryDetails table
-CREATE TABLE CountryRegionDetails (
+CREATE TABLE IF NOT EXISTS CountryRegionDetails (
     ISO3 CHAR(15) PRIMARY KEY,
     Phone VARCHAR(20),
     Region_Code VARCHAR(10),
@@ -36,8 +46,10 @@ CREATE TABLE CountryRegionDetails (
     FOREIGN KEY(ISO3) REFERENCES Countries(ISO3)
 );
 
+DROP TABLE IF EXISTS CountryDevelopmentStatus;
+
 -- Create CountryDevelopmentStatus table
-CREATE TABLE CountryDevelopmentStatus (
+CREATE TABLE IF NOT EXISTS CountryDevelopmentStatus (
     ISO3 CHAR(15) PRIMARY KEY,
     Status VARCHAR(50),
     Developed_or_Developing VARCHAR(20),
@@ -47,8 +59,10 @@ CREATE TABLE CountryDevelopmentStatus (
     FOREIGN KEY(ISO3) REFERENCES Countries(ISO3)
 );
 
+DROP TABLE IF EXISTS ASTC_Data;
+
 -- Create CountryData table
-CREATE TABLE ASTC_Data (
+CREATE TABLE IF NOT EXISTS ASTC_Data (
     ISO3 CHAR(15),
     Year INT,
     Value DOUBLE,
@@ -56,33 +70,36 @@ CREATE TABLE ASTC_Data (
     FOREIGN KEY(ISO3) REFERENCES Countries(ISO3)
 );
 
+DROP TABLE IF EXISTS CRDF_Data;
+
 -- Create CountryData table
-CREATE TABLE CRDF_Data (
+CREATE TABLE IF NOT EXISTS CRDF_Data (
     ISO3 CHAR(15),
     Indicator CHAR(20),
     Year INT,
     Value INT,
-    PRIMARY KEY (ISO3, Indicator, Year),
-    FOREIGN KEY(ISO3) REFERENCES Countries(ISO3)
+    PRIMARY KEY (ISO3, Indicator, Year)
 );
 
+DROP TABLE IF EXISTS FC_Data;
+
 -- Create CountryData table
-CREATE TABLE FC_Data (
+CREATE TABLE IF NOT EXISTS FC_Data (
     ISO3 CHAR(15),
     Indicator CHAR(35),
     Year INT,
     Value DOUBLE,
-    PRIMARY KEY (ISO3, Indicator, Year),
-    FOREIGN KEY(ISO3) REFERENCES Countries(ISO3)
+    PRIMARY KEY (ISO3, Indicator, Year)
 );
 
+DROP TABLE IF EXISTS LCA_Data;
+
 -- Create CountryData table
-CREATE TABLE LCA_Data (
+CREATE TABLE IF NOT EXISTS LCA_Data (
     ISO3 CHAR(15),
     Indicator CHAR(70),
     Influence CHAR(20),
     Year INT,
     Value DOUBLE,
-    PRIMARY KEY (ISO3, Indicator, Influence, Year),
-    FOREIGN KEY (ISO3) REFERENCES Countries(ISO3)
+    PRIMARY KEY (ISO3, Indicator, Influence, Year)
 );
